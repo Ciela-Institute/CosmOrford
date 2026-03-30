7ee163558ba12c662ceed1fb6a96c59f167767e4
Noé Dia <noedia@rorqual2.rorqual.calcul.quebec>
2026-03-30 10:39:52 -0400

    _ not -

--- shell/npe_run.sh
+++ shell/npe_run.sh
@@ -25,7 +25,7 @@ cd $WDIR
 
 python scripts/run_npe_budget_scan.py \
     --checkpoints_path=/home/noedia/links/scratch/wl_chall/budget_scan_nbody_final\
-    --npe_results_path=/home/noedia/links/scratch/wl_chall/npe-results\
+    --npe_results_path=/home/noedia/links/scratch/wl_chall/npe_results\
     --summaries_cache_path= \
     --holdout_path=/home/noedia/links/projects/rrg-lplevass/shared/wl_chall_data/neurips-wl-challenge-holdout\
     --config=configs/experiments/npe_budget_scan.yaml\
